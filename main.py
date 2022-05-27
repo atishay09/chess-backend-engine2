b238d5847ff59f09a500603000f18612867c03df
Jidnyesh <jidnyeshjawanjal29@gmail.com>
2022-05-27 16:41:54 +0530

    make it better

---
+++ main.py
@@ -0,0 +1,42 @@
+import chess
+import chess.engine
+import os
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Data(BaseModel):
+    fen: str
+    depth: int
+
+dir = os.getcwd()
+print(dir)
+engine = chess.engine.SimpleEngine.popen_uci(dir + "\stockfish_15_linux_x64\stockfish")
+
+while not board.is_game_over():
+    board.push_san(input("\nEnter your move : \n"))
+    print(board)
+    print("\n\n\nBot : \n")
+    result = engine.play(board, chess.engine.Limit(time=0.1))
+    board.push(result.move)
+    print(board)
+if board.is_game_over():
+    print("Game Over")
+
+
+@app.post("/bot/")
+def hello(data : Data):
+    board = chess.Board(data.fen)
+    if not board.is_game_over():
+        result = engine.play(board, chess.engine.Limit(time=0.1,depth = data.depth))
+        board.push(result.move)
+        return {"fen":board,"move":result.move}
+    else:
+        return {"error":"Game is in Over State"}
+
+@app.get("/")
+def hello2():
+    return {"Data":"Success"}
+    
