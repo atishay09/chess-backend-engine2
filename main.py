122406a70f01c7479c83bbefa6ef2bbd88dca65f
Jidnyesh <jidnyeshjawanjal29@gmail.com>
2022-05-27 17:15:30 +0530

    make it better

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Data(BaseModel):
 
 dir = os.getcwd()
 print(dir)
-engine = chess.engine.SimpleEngine.popen_uci(r"\app\stockfish_15_linux_x64\stockfish")
+engine = chess.engine.SimpleEngine.popen_uci(r"/app/stockfish_15_linux_x64/stockfish")
 
 
 @app.post("/bot/")
