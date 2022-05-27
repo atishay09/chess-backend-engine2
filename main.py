1bd86e1e4567f814c417538bdf5026813ad64489
Jidnyesh <jidnyeshjawanjal29@gmail.com>
2022-05-27 18:53:07 +0530

    git

--- main.py
+++ main.py
@@ -10,20 +10,28 @@ app = FastAPI()
 class Data(BaseModel):
     fen: str
     depth: int
+    class Config:
+        orm_mode = True
 
-dir = os.getcwd()
-print(dir)
 
-engine = chess.engine.SimpleEngine.popen_uci(r"/app/stockfish_15_linux_x64/stockfish")
+
+engine = chess.engine.SimpleEngine.popen_uci(r"I:\Everything\Internship\Impactional Games\stockfish_15_win_x64_avx2\stockfish.exe")
 
 
 @app.post("/bot/")
 def hello(data : Data):
-    board = chess.Board(data.fen)
+    print(data)
+    try:
+        board = chess.Board(data.fen)
+    except Exception as e:
+        return (str(e))
     if not board.is_game_over():
+        
         result = engine.play(board, chess.engine.Limit(time=0.1,depth = data.depth))
+        move = result.move
         board.push(result.move)
-        return {"fen":board,"move":result.move}
+        return {"fen":board,"move":str(move)}
+        
     else:
         return {"error":"Game is in Over State"}
 
