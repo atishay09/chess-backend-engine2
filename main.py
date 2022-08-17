db0b80fc6d1b2b9670b2f01d1726e245abcbfe7c
Atishay jain <jainatishay09sep@gmail.com>
2022-08-17 23:13:54 +0530

    engine

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import chess
 import chess.engine
 import os
 from fastapi import FastAPI
+import stat
+import sys
 
 from pydantic import BaseModel
 
@@ -13,14 +15,29 @@ class Data(BaseModel):
     class Config:
         orm_mode = True
 
+engine = ''
+
+if __name__ == '__main__':
+    dir = os.getcwd()
+    print(dir)
+    
+# engine = chess.engine.SimpleEngine.popen_uci(r"D:\internship\chess\chess-backend-engine-windows\stockfish_15_win_x64_avx2\stockfish_15_x64_avx2.exe")
+# engine = chess.engine.SimpleEngine.popen_uci(r"D:\internship\chess\chess-engine-backend1\chess-backend-engine\stockfish_15_linux_x64\stockfish.exe")
+# engine = chess.engine.SimpleEngine.popen_uci(r"/home/ubuntu/chess-backend-engine/stockfish_15_linux_x64/stockfish")
 
 
-engine = chess.engine.SimpleEngine.popen_uci(r"/home/ubuntu/chess-backend-engine/stockfish_15_linux_x64/stockfish")
 
 
 @app.post("/bot/")
 def hello(data : Data):
     print(data)
+
+    if sys.platform == "linux":
+        os.chmod("./stockfish_20011801_x64", stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+        engine = chess.engine.SimpleEngine.popen_uci("./stockfish_15_linux_x64/stockfish")
+    else:
+        engine = chess.engine.SimpleEngine.popen_uci("./stockfish_15_win_x64_avx2/stockfish_15_x64_avx2.exe")
+
     try:
         board = chess.Board(data.fen)
     except Exception as e:
